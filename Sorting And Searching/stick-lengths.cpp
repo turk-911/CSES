54903c65c3fce52a9676b562e2fec53431c7ea72
Mokshe Jain <jainmokshejain2005@gmail.com>
2025-01-24 11:47:57 +0530

    sum of two values

---
+++ Sorting And Searching/stick-lengths.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define vi vector<int>
+#define vc vector<char>
+#define pii pair<int, int>
+#define vvi vector<vi>
+#define vvc vector<vc>
+#define vpi vector<pii>
+#define vb vector<bool>
+#define vs vector<string>
+#define pb push_back
+#define mp make_pair
+#define mii map<int, int>
+#define qpii queue<pair<int, int>>
+#define spii set<pair<int, int>>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+int mod = 1000000007;
+set<int> tPrimes;
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+int csum(int a) {
+    return (a * (a + 1)) / 2;
+}
+void precalc() {
+    vb arr(1000001, false);
+    arr[0] = arr[1] = true;
+    for (int i = 2; i * i < 1000001; i++) {
+        if (!arr[i]) {
+            for (int j = i * i; j < 1000001; j += i) {
+                arr[j] = true;
+            }
+        }
+    }
+    for (int i = 0; i < 1000001; i++) {
+        if (!arr[i]) tPrimes.insert(i * i);
+    }
+}
+int nCr(int n, int r, vector<vector<int>> &dp) {
+    if (r > n) return 0;
+    if (r == 0 || r == n) return 1;
+    if(dp[n][r] != -1) return dp[n][r];
+    return dp[n][r] = nCr(n-1, r, dp) + nCr(n-1, r-1, dp);
+}
+
+int power(int x, unsigned int y, int p) {
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+string DecimalToBinary(int num) {
+    string str;
+    while (num) {
+        if (num & 1) str += '1';
+        else str += '0';
+        num >>= 1;
+    }
+    return str;
+}
+int factorial(int a) {
+    int p = 1;
+    for (int i = 2; i <= a; i++) p = p * i;
+    return p;
+}
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    int avg_sum = 0;
+    for(int i = 0; i < n; i++) avg_sum += arr[i];
+    avg_sum /= n;
+    if(avg_sum % n) avg_sum++;
+    int cost = 0;
+    for(int i = 0; i < n; i++) cost += abs(avg_sum - arr[i]);
+    cout << cost << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    // precalc();
+    for (int i = 1; i <= t; i++) solve();
+    return 0;
+}
