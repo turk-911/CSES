c1bc09c452f7f62fade9df1c2909a1266928a60c
turk-911 <jainmokshejain2005@gmail.com>
2025-02-22 17:50:31 +0530

    high score

---
+++ Graphs/high-score.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+int mod = 1000000007;
+set<int> tPrimes;
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+int csum(int a) {
+    return (a * (a + 1)) / 2;
+}
+void precalc() {
+    vector<bool> arr(1000001, false);
+    arr[0] = arr[1] = true;
+    for (int i = 2; i * i < 1000001; i++) {
+        if (!arr[i]) {
+            for (int j = i * i; j < 1000001; j += i) {
+                arr[j] = true;
+            }
+        }
+    }
+    for (int i = 0; i < 1000001; i++) {
+        if (!arr[i]) tPrimes.insert(i * i);
+    }
+}
+int nCr(int n, int r, vector<vector<int>> &dp) {
+    if (r > n) return 0;
+    if (r == 0 || r == n) return 1;
+    if(dp[n][r] != -1) return dp[n][r];
+    return dp[n][r] = nCr(n-1, r, dp) + nCr(n-1, r-1, dp);
+}
+
+int power(int x, unsigned int y, int p) {
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+string DecimalToBinary(int num) {
+    string str;
+    while (num) {
+        if (num & 1) str += '1';
+        else str += '0';
+        num >>= 1;
+    }
+    return str;
+}
+int factorial(int a) {
+    int p = 1;
+    for (int i = 2; i <= a; i++) p = p * i;
+    return p;
+}
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> u(m), v(m), wt(m);
+    for(int i = 0; i < m; i++) {
+        cin >> u[i] >> v[i] >> wt[i];
+    }
+    vector<int> dist(n + 1, -1e18);
+    dist[1] = 0;
+    for(int i = 1; i <= n - 1; i++) {
+        for(int j = 0; j < m; j++) {
+            if(dist[u[j]] != -1e18) {
+                dist[v[j]] = max(dist[v[j]], dist[u[j]] + wt[j]);
+            }
+        } 
+    }
+    vector<bool> reachable(n + 1, false);
+    queue<int> q;
+    vector<bool> inQueue(n + 1, false);
+    for(int j = 0; j < m; j++) {
+        if(dist[u[j]] != -1e18 && dist[v[j]] < dist[u[j]] + wt[j]) {
+            reachable[v[j]] = true;
+            if(inQueue[v[j]] == false) {
+                q.push(v[j]);
+                inQueue[v[j]] = true;
+            }
+        }
+    }
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        for(int j = 0; j < m; j++) {
+            if(u[j] == node && reachable[v[j]] == false) {
+                reachable[v[j]] = true;
+                q.push(v[j]);
+            }
+        }
+    }
+    if(reachable[n] == true) {
+        cout << -1 << endl;
+    }
+    else {
+        cout << dist[n] << endl;
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    // precalc();
+    for (int i = 1; i <= t; i++) solve();
+    return 0;
+}
