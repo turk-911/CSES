64853b7b8a91274cfc60219be29de408ddee6090
turk-911 <jainmokshejain2005@gmail.com>
2025-03-22 16:16:05 +0530

    word combinations

---
+++ String Algorithms/word-combinations.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+int mod = 1000000007;
+set<int> tPrimes;
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+int csum(int a) {
+    return (a * (a + 1)) / 2;
+}
+void precalc() {
+    vector<bool> arr(1000001, false);
+    arr[0] = arr[1] = true;
+    for (int i = 2; i * i < 1000001; i++) {
+        if (!arr[i]) {
+            for (int j = i * i; j < 1000001; j += i) {
+                arr[j] = true;
+            }
+        }
+    }
+    for (int i = 0; i < 1000001; i++) {
+        if (!arr[i]) tPrimes.insert(i * i);
+    }
+}
+int nCr(int n, int r, vector<vector<int>> &dp) {
+    if (r > n) return 0;
+    if (r == 0 || r == n) return 1;
+    if(dp[n][r] != -1) return dp[n][r];
+    return dp[n][r] = nCr(n-1, r, dp) + nCr(n-1, r-1, dp);
+}
+
+int power(int x, unsigned int y, int p) {
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+string DecimalToBinary(int num) {
+    string str;
+    while (num) {
+        if (num & 1) str += '1';
+        else str += '0';
+        num >>= 1;
+    }
+    return str;
+}
+struct Node {
+    unordered_map<char, Node*> children;
+    bool is_end;
+    Node(): is_end(false) {}
+};
+class Trie {
+    public: 
+    Node *root;
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(const string &word) {
+        Node *node = root;
+        for(char ch: word) {
+            if(!node->children.count(ch)) {
+                node->children[ch] = new Node();
+            }
+            node = node->children[ch];
+        }
+        node->is_end = true;
+    }
+    
+    vector<int> search(const string &s, int start) {
+        vector<int> valid_endings;
+        Node *node = root;
+        int sz = s.length();
+        for(int i = start; i < sz; i++) {
+            if(!node->children.count(s[i])) {
+                break;
+            }
+            node = node->children[s[i]];
+            if(node->is_end) {
+                valid_endings.push_back(i+1);
+            }
+        }
+        return valid_endings;
+    }
+};
+void solve() {
+    Trie trie;
+    string s;
+    cin >> s;
+    int n = s.length();
+    
+    int k;
+    cin >> k;
+    
+    vector<string> dict(k);
+    for(int i = 0; i < k; i++) {
+        cin >> dict[i];
+    }
+    
+    for(string &word: dict) {
+        trie.insert(word);
+    }
+    
+    vector<int> dp(n+1, 0);
+    dp[0] = 1;
+    
+    for(int i = 0; i < n; i++) {
+        if(dp[i] == 0) {
+            continue;
+        }
+        vector<int> endings = trie.search(s, i);
+        for(int end: endings) {
+            dp[end] = (dp[end]+dp[i]) % mod;
+        }
+    }
+    
+    cout << dp[n];
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    // precalc();
+    for (int i = 1; i <= t; i++) solve();
+    return 0;
+}
