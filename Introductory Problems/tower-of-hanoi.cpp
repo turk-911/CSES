82d61285f6defd0944521f5898f6f15b95862024
turk-911 <jainmokshejain2005@gmail.com>
2025-03-22 17:51:01 +0530

    tower of hanoi

---
+++ Introductory Problems/tower-of-hanoi.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+int mod = 1000000007;
+set<int> tPrimes;
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+int csum(int a) {
+    return (a * (a + 1)) / 2;
+}
+void precalc() {
+    vector<bool> arr(1000001, false);
+    arr[0] = arr[1] = true;
+    for (int i = 2; i * i < 1000001; i++) {
+        if (!arr[i]) {
+            for (int j = i * i; j < 1000001; j += i) {
+                arr[j] = true;
+            }
+        }
+    }
+    for (int i = 0; i < 1000001; i++) {
+        if (!arr[i]) tPrimes.insert(i * i);
+    }
+}
+int nCr(int n, int r, vector<vector<int>> &dp) {
+    if (r > n) return 0;
+    if (r == 0 || r == n) return 1;
+    if(dp[n][r] != -1) return dp[n][r];
+    return dp[n][r] = nCr(n-1, r, dp) + nCr(n-1, r-1, dp);
+}
+
+int power(int x, unsigned int y, int p) {
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+string DecimalToBinary(int num) {
+    string str;
+    while (num) {
+        if (num & 1) str += '1';
+        else str += '0';
+        num >>= 1;
+    }
+    return str;
+}
+int factorial(int a) {
+    int p = 1;
+    for (int i = 2; i <= a; i++) p = p * i;
+    return p;
+}
+void resolve(int n, int from, int to, int auxillary) {
+    if(n == 0) {
+        return;
+    }
+    resolve(n-1, from, auxillary, to);
+    cout << from << " " << to << endl;
+    resolve(n-1, auxillary, to, from);
+}
+void solve() {
+    int n;
+    cin >> n;
+    
+    int moves = (1<<n)-1;
+    cout << moves << endl;
+    
+    resolve(n, 1, 3, 2);
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    // precalc();
+    for (int i = 1; i <= t; i++) solve();
+    return 0;
+}
