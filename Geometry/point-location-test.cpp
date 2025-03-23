65ed28bc6b0375ae1c0886662eebcc2d20053d84
turk-911 <jainmokshejain2005@gmail.com>
2025-03-23 19:06:50 +0530

    point location test

---
+++ Geometry/point-location-test.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+int mod = 1000000007;
+set<int> tPrimes;
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+int csum(int a) {
+    return (a * (a + 1)) / 2;
+}
+void precalc() {
+    vector<bool> arr(1000001, false);
+    arr[0] = arr[1] = true;
+    for (int i = 2; i * i < 1000001; i++) {
+        if (!arr[i]) {
+            for (int j = i * i; j < 1000001; j += i) {
+                arr[j] = true;
+            }
+        }
+    }
+    for (int i = 0; i < 1000001; i++) {
+        if (!arr[i]) tPrimes.insert(i * i);
+    }
+}
+int nCr(int n, int r, vector<vector<int>> &dp) {
+    if (r > n) return 0;
+    if (r == 0 || r == n) return 1;
+    if(dp[n][r] != -1) return dp[n][r];
+    return dp[n][r] = nCr(n-1, r, dp) + nCr(n-1, r-1, dp);
+}
+
+int power(int x, unsigned int y, int p) {
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+string DecimalToBinary(int num) {
+    string str;
+    while (num) {
+        if (num & 1) str += '1';
+        else str += '0';
+        num >>= 1;
+    }
+    return str;
+}
+int factorial(int a) {
+    int p = 1;
+    for (int i = 2; i <= a; i++) p = p * i;
+    return p;
+}
+void solve() {
+    int x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    
+    int deter = ((x2-x1) * (y3-y1)) - ((y2-y1) * (x3-x1));
+    
+    if(deter > 0) {
+        cout << "LEFT" << endl;
+    }
+    else if(deter < 0) {
+        cout << "RIGHT" << endl;
+    }
+    else {
+        cout << "TOUCH" << endl;
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    // precalc();
+    for (int i = 1; i <= t; i++) solve();
+    return 0;
+}
